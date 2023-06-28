4eb26c8d383ce0b22ea802dbfc67451391c8aa8d
dariom25 <dario@maciey.de>
2023-06-28 18:44:12 +0200

    Add function to get players choice

--- logic.js
+++ logic.js
@@ -1,4 +1,4 @@
-const availableChoices = ["Rock", "Paper", "Scissors"]
+const availableChoices = ["rock", "paper", "scissors"]
 
 function getComputerChoice(availableChoices) {
     let randomIndex = Math.floor(Math.random() * availableChoices.length);
@@ -6,7 +6,14 @@ function getComputerChoice(availableChoices) {
     return computersChoice
 }
 
+function getPlayersChoice(availableChoices) {
+    let playersChoice = prompt("What do you choose? rock, paper or scissors?");
+    console.log(playersChoice)
+}
+
 //get the computers choice
 //get the players choice
+    //lower all letters of the players prompt
+    //check if the the players answer is valid
 //compare the computers and players choice
 //determine who wins
