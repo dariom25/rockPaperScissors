be5c9a5ad206c58715fde6788ab6988c72c4097e
dariom25 <dario@maciey.de>
2023-07-09 18:01:29 +0200

    Add files

---
+++ script.js
@@ -0,0 +1,9 @@
+const buttons = document.getElementsByClassName('button');
+console.log(buttons)
+console.log("Test")
+// we use the .forEach method to iterate through each button
+for (let button of buttons) {
+  console.log(button.id);
+}
+
+https://themeisle.com/blog/how-to-host-a-website/
